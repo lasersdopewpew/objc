738f08366516db68ab181a646e0d9184b6e16e1b
apas <apostolos.me@gmail.com>
2013-03-29 21:48:45 +0100

    cleaned the code

--- Ex5p147/main.m
+++ Ex5p147/main.m
@@ -3,21 +3,12 @@
 int main(int argc, const char * argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     Fraction *aFraction = [[Fraction alloc] init];
-    Fraction *bFraction = [[Fraction alloc] init]; //*sum2;
     
     [aFraction setTo: 10 over: 3];
-    [bFraction setTo: -1 over: 2];
     
     [aFraction print: FALSE];
     
-  //  sum2 = [aFraction add: bFraction];
-  //  [sum2 print: TRUE];
-    //when 'TRUE' there's a bug when f.numerator is negative and instead of -numerator/denominator it prints numerator/-denominator - the correct end-result, just with the
-    //minus 'swapped'. in any other permutation of TRUE and minuses, when setting the fractions, everything's OK--as well as when print takes FALSE.
-    
     [aFraction release];
-    [bFraction release];
-   // [sum2 release];
     [pool drain];
     return 0;
 }
